4aafb65baa715d4a3e98ec957b61033bd93a20ed
Mohammad1745 <mdali2016.227@gmail.com>
2022-11-09 00:24:46 +0600

    [Change] absolute settings

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
 document.addEventListener('DOMContentLoaded', () => {
     let sections = document.querySelectorAll('.section')
+    let settings = document.getElementById('settings')
     for (let i=0; i<sections.length; i++) {
-        sections[i].addEventListener('mouseover', event => {
-            let settingsDom = document.getElementById('settings')
-            if (settingsDom) settingsDom.remove()
-            let settingsContent = `
-                <div class="settings" id="settings">
-                    <button class="button">B1</button>
-                    <button class="button">B2</button>
-                    <button class="button">B3</button>
-                </div>
-            `
-            sections[i].insertAdjacentHTML('afterbegin', settingsContent)
+        sections[i].addEventListener('mouseenter', event => {
+            let x  = sections[i].offsetLeft
+            let y  = sections[i].offsetTop
+            settings.style.right = x+'px'
+            settings.style.top = y+'px'
+            settings.classList.remove('d-none')
+        })
+        sections[i].addEventListener('mouseleave', event => {
+            settings.classList.add('d-none')
         })
     }
 })
